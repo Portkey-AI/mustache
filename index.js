dc639d1c2d08a8981bb104ffb4b1bfe8a3e07cbd
visargD <visargdesai@gmail.com>
2024-03-14 13:14:57 +0530

    fix: remove escape html function

--- index.js
+++ index.js
@@ -868,7 +868,9 @@ mustache.getTemplateDetails = function (template, tags) {
 
 // Export the escaping function so that the user may override it.
 // See https://github.com/janl/mustache.js/issues/244
-mustache.escape = escapeHtml;
+mustache.escape = function(string) {
+  return string;
+};;
 
 // Export these mainly for testing, but also for advanced usage.
 mustache.Scanner = Scanner;
